9d54f2ae4dadc75fae21a10220a4c4ba5d88b122
hunarjain <hunarjain007@gmail.com>
2020-11-29 19:21:01 +0530

    textInput

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#text-input");
+btnTranslate.addEventListener("click",()=>{
+    console.log("clicked");
+    console.log(textInput.value);
+});
