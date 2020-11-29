789d700d0628bd340775b02d1e89c364116fa1a7
hunarjain <hunarjain007@gmail.com>
2020-11-29 20:59:44 +0530

    added minion api

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var outputText = document.querySelector("#output-text");
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 
 function getTranslationURL(text){
